4f3855dc1d2e3f41d73429ed90743822c35fc290
Wejdi <wejdigharbi@yahoo.fr>
2019-03-12 23:11:09 +0100

    Updates

---
+++ precache-manifest.d4e504b95d23bec48bb53a35cbc7cfc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cd5e06e8dcf926a914fc5b1573315ec",
+    "url": "/Complex-Credit-Card-React/static/media/GC16-Omni-4_1.7cd5e06e.jpg"
+  },
+  {
+    "revision": "e00780abed8290a0e2304b6561efe09d",
+    "url": "/Complex-Credit-Card-React/static/media/Capture2.e00780ab.PNG"
+  },
+  {
+    "revision": "95bfbd13afe094cffe1af70945c7774f",
+    "url": "/Complex-Credit-Card-React/static/media/Capture.95bfbd13.PNG"
+  },
+  {
+    "revision": "79af68c582644570ca1f",
+    "url": "/Complex-Credit-Card-React/static/js/runtime~main.79af68c5.js"
+  },
+  {
+    "revision": "1e4d920792d9107a3e93",
+    "url": "/Complex-Credit-Card-React/static/js/main.1e4d9207.chunk.js"
+  },
+  {
+    "revision": "a2e495542c5b35fde0dc",
+    "url": "/Complex-Credit-Card-React/static/js/2.a2e49554.chunk.js"
+  },
+  {
+    "revision": "1e4d920792d9107a3e93",
+    "url": "/Complex-Credit-Card-React/static/css/main.50beb60b.chunk.css"
+  },
+  {
+    "revision": "8ef8f00be15e0d3f13088b5f197f34a6",
+    "url": "/Complex-Credit-Card-React/index.html"
+  }
+];
